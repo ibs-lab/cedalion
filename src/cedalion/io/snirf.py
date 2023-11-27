3fabd202eaab497c15fc79a0cdc7c96370a7bd5c
Eike Middell <eike@middell.net>
2023-11-27 09:47:40 +0100

    writing snirf files. handling of nd-aux signals

--- src/cedalion/io/snirf.py
+++ src/cedalion/io/snirf.py
@@ -7,6 +7,8 @@ from enum import Enum
 import logging
 from numpy.typing import ArrayLike
 from functools import total_ordering
+from typing import Dict
+
 
 log = logging.getLogger("cedalion")
 
@@ -55,14 +57,25 @@ class PointType(Enum):
 # the data in the attributes have been converted to our data structures
 # FIXME: find a better name
 class Element:
-    data: list
+    # data: list
 
-    def __init__(self, data=None, stim=None, geo3d=None, geo2d=None, aux=None):
+    def __init__(
+        self,
+        data=None,
+        stim=None,
+        geo3d=None,
+        geo2d=None,
+        aux=None,
+        meta_data=None,
+        measurement_lists=None,
+    ):
         self.data = []
         self.stim = pd.DataFrame(columns=["onset", "duration", "value"])
         self.geo3d = xr.DataArray(None, coords={"label": []}, dims=["label", "pos"])
         self.geo2d = xr.DataArray(None, coords={"label": []}, dims=["label", "pos"])
         self.aux = {}
+        self.meta_data = {}
+        self.measurement_lists = []
 
         if data is not None:
             self.data = data
@@ -74,6 +87,10 @@ class Element:
             self.geo2d = geo2d
         if aux is not None:
             self.aux = aux
+        if meta_data is not None:
+            self.meta_data = meta_data
+        if measurement_lists is not None:
+            self.measurement_lists = measurement_lists
 
 
 def reduce_ndim_sourceLabels(sourceLabels: np.ndarray) -> list:
@@ -130,11 +147,11 @@ def labels_and_positions(probe):
 
     if len(sourcePos3D) > 0 and len(sourceLabels) == 0:
         log.warn("generating generic source labels")
-        sourceLabels = np.asarray([f"S{i}" for i in range(len(sourcePos3D))])
+        sourceLabels = np.asarray([f"S{i+1}" for i in range(len(sourcePos3D))])
 
     if len(detectorPos3D) > 0 and len(detectorLabels) == 0:
         log.warn("generating generic detector labels")
-        detectorLabels = np.asarray([f"D{i}" for i in range(len(detectorPos3D))])
+        detectorLabels = np.asarray([f"D{i+1}" for i in range(len(detectorPos3D))])
 
     if len(landmarkLabels) != len(landmarkPos3D):
         raise ValueError("landmark positions were provided but no labels")
@@ -217,6 +234,14 @@ def measurement_list_to_dataframe(
     return df
 
 
+def meta_data_tags_to_dict(nirs_element: NirsElement):
+    mdt = nirs_element.metaDataTags
+
+    fields = mdt._snirf_names + mdt._unspecified_names
+
+    return {f: getattr(mdt, f) for f in fields}
+
+
 def stim_to_dataframe(stim):
     dfs = []
 
@@ -250,17 +275,25 @@ def read_aux(nirs_element: NirsElement):
     for aux in nirs_element.aux:
         name = aux.name
         units = aux.dataUnit
+        time_offset = aux.timeOffset
 
         # FIXME treat unspecified units as dimensionless quantities.
         if units is None:
             units = "1"
 
+        if aux.dataTimeSeries.ndim == 1:
+            dims = ["time"]
+        elif aux.dataTimeSeries.ndim == 2:
+            dims = ["time", "aux_channel"]
+        else:
+            raise ValueError("aux.dataTimeSeries must have either 1 or 2 dimensions.")
+
         x = xr.DataArray(
             aux.dataTimeSeries,
-            coords={"time": aux.time},  # FIXME time offset
-            dims=["time"],
+            coords={"time": aux.time},
+            dims=dims,
             name=name,
-            attrs={"units": units},
+            attrs={"units": units, "time_offset": time_offset},
         )
         result[name] = x.pint.quantify()
 
@@ -342,99 +375,44 @@ def read_data_element(data_element, nirs_element):
     return da
 
 
-def _get_time_index_and_coords(
+def _get_time_coords(
     nirs_element: NirsElement,
     data_element: DataElement,
     df_measurement_list: pd.DataFrame,
-) -> tuple[ArrayLike, dict[str, ArrayLike]]:
+) -> dict[str, ArrayLike]:
     time = data_element.time
     time_unit = nirs_element.metaDataTags.TimeUnit
-
-    indices = np.arange(len(time))
 
     if len(time) != data_element.dataTimeSeries.shape[0]:
         raise ValueError("length of time and dataTimeSeries arrays don't match!")
 
     coordinates = {
         "time": ("time", xr.DataArray(time, attrs={"units": time_unit})),
-        "samples": ("samples", np.arange(len(time))),
+        "samples": ("time", np.arange(len(time))),
     }
-
+    indices = None
     return indices, coordinates
 
 
-def _get_channel_index_and_coords(
+def _get_channel_coords(
     nirs_element: NirsElement,
-    data_element: DataElement,
     df_measurement_list: pd.DataFrame,
 ) -> tuple[ArrayLike, dict[str, ArrayLike]]:
     sourceLabels, detectorLabels, landmarkLabels, _, _, _ = labels_and_positions(
         nirs_element.probe
     )
 
-    # for each channel in the measurement list build a channel label
-    df_channels = pd.DataFrame(
-        [
-            (f"{sl}{dl}", sl, dl)
-            for sl, dl in zip(
-                sourceLabels[df_measurement_list.sourceIndex - 1],
-                detectorLabels[df_measurement_list.detectorIndex - 1],
-            )
-        ],
-        columns=["channel_label", "source", "detector"],
+    df_measurement_list = denormalize_measurement_list(
+        df_measurement_list, nirs_element
     )
 
-    # reduce channels to unique source-detector combinations
-    df_unique_channels = df_channels.drop_duplicates(ignore_index=True)
-    unique_channel_labels = df_unique_channels["channel_label"].tolist()
-
-    indices = [
-        unique_channel_labels.index(label) for label in df_channels["channel_label"]
-    ]
-
     coordinates = {
-        "channel": ("channel", df_unique_channels["channel_label"]),
-        "source": ("channel", df_unique_channels["source"]),
-        "detector": ("channel", df_unique_channels["detector"]),
+        "channel": ("channel", df_measurement_list["channel"]),
+        "source": ("channel", df_measurement_list["source"]),
+        "detector": ("channel", df_measurement_list["detector"]),
     }
-
+    indices = None
     return indices, coordinates
-
-
-# def read_data_element_2(
-#    data_element: DataElement, nirs_element: NIRSElement
-# ) -> xr.DataArray:
-#    """TBD."""
-#    df_ml = measurement_list_to_dataframe(data_element.measurementList)
-#
-#    unique_data_types = df_ml["dataType"].unique()
-#    if len(unique_data_types) > 1:
-#        raise ValueError(
-#            f"data element at {data_element.location} contains multiple data types"
-#        )
-#
-#    data_type = DataType(unique_data_types.item())
-#
-#    match data_type:
-#        case DataType.CW_AMPLITUDE:
-#            dims = ["channel", "wavelength", "time"]
-#        case DataType.TDM_AMPLITUDE:
-#            dims = ["channel", "wavelength", "moment", "time"]
-#            pass
-#        case DataType.PROCESSED:
-#            pass
-#        case _:
-#            raise NotImplementedError(f"data type {data_type} is not yet supported.")
-#
-#    time = data_element.time
-#    samples = np.arange(len(time))
-#
-#    if len(time) != data_element.dataTimeSeries.shape[0]:
-#        raise ValueError("length of time and dataTimeSeries arrays don't match!")
-#
-#    sourceLabels, detectorLabels, landmarkLabels, _, _, _ = labels_and_positions(
-#        nirs_element.probe
-#    )
 
 
 def read_nirs_element(nirs_element):
@@ -447,7 +425,19 @@ def read_nirs_element(nirs_element):
 
     aux = read_aux(nirs_element)
 
-    ne = Element(data, stim, geo3d, None, aux)  # FIXME geo2d
+    meta_data = meta_data_tags_to_dict(nirs_element)
+
+    measurement_lists = []
+    for data_element in nirs_element.data:
+        df_ml = measurement_list_to_dataframe(
+            data_element.measurementList, drop_none=True
+        )
+        df_ml = denormalize_measurement_list(df_ml, nirs_element)
+        measurement_lists.append(df_ml)
+
+    ne = Element(
+        data, stim, geo3d, None, aux, meta_data, measurement_lists
+    )  # FIXME geo2d
 
     return ne
 
@@ -455,3 +445,180 @@ def read_nirs_element(nirs_element):
 def read_snirf(fname):
     with Snirf(fname, "r") as s:
         return [read_nirs_element(ne) for ne in s.nirs]
+
+
+def denormalize_measurement_list(df_ml: pd.DataFrame, nirs_element: NirsElement):
+    sourceLabels, detectorLabels, landmarkLabels, _, _, _ = labels_and_positions(
+        nirs_element.probe
+    )
+    wavelengths = nirs_element.probe.wavelengths
+
+    if (df_ml.sourceIndex < 1).any() or (df_ml.detectorIndex < 1).any():
+        raise ValueError(
+            "measurement list has row(s) for which source or detector index < 1."
+        )
+
+    new_columns = []
+    for _, row in df_ml.iterrows():
+        sl = sourceLabels[int(row["sourceIndex"]) - 1]
+        dl = detectorLabels[int(row["detectorIndex"]) - 1]
+        cl = f"{sl}{dl}"
+        if row["wavelengthIndex"] != -1:
+            wl = wavelengths[int(row["wavelengthIndex"]) - 1]
+        else:
+            wl = np.nan
+
+        new_columns.append((cl, sl, dl, wl))
+
+    new_columns = pd.DataFrame(
+        new_columns, columns=["channel", "source", "detector", "wavelength"]
+    )
+    result = pd.concat((df_ml, new_columns), axis="columns")
+    return result
+
+
+def measurement_list_from_stacked(
+    stacked_array, data_type, stacked_channel="snirf_channel"
+):
+    source_labels = list(np.unique(stacked_array.source.values))
+    detector_labels = list(np.unique(stacked_array.detector.values))
+    wavelengths = []
+
+    if "wavelength" in stacked_array.coords:
+        wavelengths = list(np.unique(stacked_array.wavelength.values))
+
+    nchannel = stacked_array.sizes[stacked_channel]
+
+    ml = dict()
+    ml["sourceIndex"] = [
+        source_labels.index(s) + 1 for s in stacked_array.source.values
+    ]
+    ml["detectorIndex"] = [
+        detector_labels.index(d) + 1 for d in stacked_array.detector.values
+    ]
+
+    if data_type == "amplitude":
+        ml["dataType"] = [DataType.CW_AMPLITUDE.value] * nchannel
+    elif data_type == "od":
+        ml["dataType"] = [DataType.PROCESSED.value] * nchannel
+        ml["dataTypeLabel"] = ["dOD"] * nchannel
+    elif data_type == "concentration":
+        ml["dataType"] = [DataType.PROCESSED.value] * nchannel
+        ml["dataTypeLabel"] = stacked_array.chromo.values
+
+    if "wavelength" in stacked_array.coords:
+        wavelengths = list(np.unique(stacked_array.wavelength.values))
+        ml["wavelengthIndex"] = [
+            wavelengths.index(w) + 1 for w in stacked_array.wavelength.values
+        ]
+    else:
+        ml["wavelengthIndex"] = [-1] * nchannel
+
+    ml["dataUnit"] = [stacked_array.attrs["units"]] * nchannel
+
+    return source_labels, detector_labels, wavelengths, pd.DataFrame(ml)
+
+
+def write_snirf(
+    fname: str,
+    data_type: str,
+    timeseries: xr.DataArray,
+    geo3d: xr.DataArray,
+    stim: pd.DataFrame,
+    measurement_list: pd.DataFrame,
+    aux: Dict[str, xr.DataArray] = {},
+    meta_data: Dict = {},
+):
+    if data_type not in ["amplitude", "od", "concentration"]:
+        raise ValueError(
+            "data_type must be either 'amplitude', 'od' or 'concentration'."
+        )
+
+    assert timeseries.ndim == 3
+
+    assert "channel" in timeseries.dims
+    assert "time" in timeseries.dims
+
+    other_dim = None
+
+    if "wavelength" in timeseries.dims:
+        other_dim = "wavelength"
+    elif "chromo" in timeseries.dims:
+        other_dim = "chromo"
+    else:
+        raise ValueError(
+            "expect 3-dim timeseries with either 'wavelength' or 'chromo' dimensions"
+        )
+
+    stacked_array = timeseries.stack({"snirf_channel": ["channel", other_dim]})
+    stacked_array = stacked_array.transpose("time", "snirf_channel")
+    stacked_array = stacked_array.pint.dequantify()
+
+    source_labels, detector_labels, wavelengths, df_ml = measurement_list_from_stacked(
+        stacked_array, data_type
+    )
+
+    geo3d = geo3d.pint.dequantify()
+
+    with Snirf(fname, "w") as fout:
+        # create and populate nirs element
+        fout.nirs.appendGroup()
+        ne = fout.nirs[0]
+
+        # set meta data
+        for k, v in meta_data.items():
+            setattr(ne.metaDataTags, k, v)
+
+        ne.metaDataTags.LengthUnit = geo3d.attrs["units"]
+
+        ne.probe.sourceLabels = source_labels
+        ne.probe.detectorLabels = detector_labels
+        ne.probe.wavelengths = wavelengths
+
+        ne.probe.sourcePos3D = geo3d.loc[source_labels]
+        ne.probe.detectorPos3D = geo3d.loc[detector_labels]
+
+        # create and populate data element
+        ne.data.appendGroup()
+        data = ne.data[0]
+
+        data.dataTimeSeries = stacked_array.values
+        data.time = stacked_array.time.values
+
+        # build measurement list
+        for i, row in df_ml.iterrows():
+            data.measurementList.appendGroup()
+            ml = data.measurementList[-1]
+            for k, v in row.items():
+                setattr(ml, k, v)
+
+        # save stimulus
+        for trial_type, df in stim.groupby("trial_type"):
+            ne.stim.appendGroup()
+            stim_group = ne.stim[-1]
+            df = df.drop(columns="trial_type")
+            assert all(df.columns[:3] == ["onset", "duration", "value"])
+
+            stim_group.data = df.values
+            stim_group.name = trial_type
+
+            if len(df.columns) > 3:
+                stim_group.dataLabels = list(df.columns[3:])
+
+        # save aux
+
+        for aux_name, aux_array in aux:
+            ne.aux.appendGroup()
+            aux_group = ne.aux[-1]
+
+            aux_array = aux_array.pint.dequantify()
+
+            aux_group.name = aux_name
+            aux_group.dataTimeSeries = aux_array
+            aux_group.dataUnit = aux_array.attrs[
+                "units"
+            ]  # FIXME add checks that time units match those in metaDataTags
+            aux_group.time = aux_array.time
+            aux_group.timeOffset = aux_array.attrs["time_offset"]
+
+        fout.save()
