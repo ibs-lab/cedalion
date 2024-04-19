0547a575837d9a2958e5811a700d0db2b354c175
Alexander von Lühmann <avonluh@gmail.com>
2024-04-19 19:09:05 +0200

    added docstrings for nirs.py

--- src/cedalion/nirs.py
+++ src/cedalion/nirs.py
@@ -9,7 +9,24 @@ from importlib import resources
 
 
 def get_extinction_coefficients(spectrum: str, wavelengths: ArrayLike):
-    """Provide a matrix of extinction coefficients from tabulated data."""
+    """Provide a matrix of extinction coefficients from tabulated data.
+
+    Args:
+        spectrum (str): The type of spectrum to use. Currently supported options are:
+            - "prahl": Extinction coefficients based on the Prahl absorption spectrum.
+        wavelengths (ArrayLike): An array-like object containing the wavelengths at which
+            to calculate the extinction coefficients.
+
+    Returns:
+        xr.DataArray: A matrix of extinction coefficients with dimensions "chromo"
+        (chromophore) and "wavelength", where "chromo" represents the type of chromophore
+        (e.g., "HbO", "HbR") and "wavelength" represents the wavelengths at which the
+        extinction coefficients are calculated. The returned data array is annotated
+        with units of "mm^-1 / M".
+
+    References:
+        (:cite:t:`Prahl1998`)
+    """
     if spectrum == "prahl":
         with resources.open_text(
             "cedalion.data", "prahl_absorption_spectrum.tsv"
@@ -38,7 +55,16 @@ def get_extinction_coefficients(spectrum: str, wavelengths: ArrayLike):
 
 
 def channel_distances(amplitudes: xr.DataArray, geo3d: xr.DataArray):
-    """Calculate distances between channels."""
+    """Calculate distances between channels.
+
+    Args:
+        amplitudes (xr.DataArray): A DataArray representing the amplitudes with dimensions (channel, *).
+        geo3d (xr.DataArray): A DataArray containing the 3D coordinates of the channels with dimensions (channel, pos).
+
+    Returns:
+        dists (xr.DataArray): A DataArray containing the calculated distances between source and detector channels. 
+            The resulting DataArray has the dimension 'channel'.
+    """
     validators.has_channel(amplitudes)
     validators.has_positions(geo3d, npos=3)
     validators.is_quantified(geo3d)
@@ -51,7 +77,14 @@ def channel_distances(amplitudes: xr.DataArray, geo3d: xr.DataArray):
 
 
 def int2od(amplitudes: xr.DataArray):
-    """Calculate optical density from intensity amplitude  data."""
+    """Calculate optical density from intensity amplitude  data.
+
+    Args:
+        amplitudes (xr.DataArray, (time, channel, *)): amplitude data.
+
+    Returns:
+        od: (xr.DataArray, (time, channel,*): The optical density data.
+    """
     od = - np.log( amplitudes / amplitudes.mean("time") )
     return od
 
@@ -62,7 +95,19 @@ def od2conc(
     dpf: xr.DataArray,
     spectrum: str = "prahl",
 ):
-    """Calculate concentration changes from optical density data."""
+    """Calculate concentration changes from optical density data.
+
+    Args:
+        od (xr.DataArray, (channel, wavelength, *)): The optical density data array
+        geo3d (xr.DataArray): The 3D coordinates of the optodes.
+        dpf (xr.DataArray, (wavelength, *)): The differential pathlength factor data
+        spectrum (str, optional): The type of spectrum to use for calculating extinction
+            coefficients. Defaults to "prahl".
+
+    Returns:
+        conc: A data array containing concentration changes with dimensions
+        "channel" and "wavelength".
+    """
     validators.has_channel(od)
     validators.has_wavelengths(od)
     validators.has_wavelengths(dpf)
@@ -78,7 +123,7 @@ def od2conc(
     # conc = Einv @ (optical_density / ( dists * dpf))
     conc = xr.dot(Einv, od / (dists * dpf), dims=["wavelength"])
     conc = conc.pint.to("micromolar")
-    conc = conc.rename("concentrations")
+    conc = conc.rename("concentration")
 
     return conc
 
@@ -89,7 +134,19 @@ def beer_lambert(
     dpf: xr.DataArray,
     spectrum: str = "prahl",
 ):
-    """Calculate concentration changes from amplitude data."""
+    """Calculate concentration changes from amplitude data using the modified beer-lambert law.
+
+    Args:
+        amplitudes (xr.DataArray, (channel, wavelength, *)): The input data array containing the raw intensities.
+        geo3d (xr.DataArray): The 3D coordinates of the optodes.
+        dpf (xr.DataArray, (wavelength,*)): The differential pathlength factors
+        spectrum (str, optional): The type of spectrum to use for calculating extinction
+            coefficients. Defaults to "prahl".
+
+    Returns:
+        conc (xr.DataArray, (channel, wavelength, *)): A data array containing concentration 
+            changes according to the mBLL
+    """
     validators.has_channel(amplitudes)
     validators.has_wavelengths(amplitudes)
     validators.has_wavelengths(dpf)
