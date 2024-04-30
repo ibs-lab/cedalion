1f47307a83944d3a1567d1d58b946bfe46a8ca1c
Eike Middell <eike@middell.net>
2024-04-30 10:51:52 +0200

    fixes test_opt_fid_to_xr for empty coordinate lists.

--- src/cedalion/io/photogrammetry.py
+++ src/cedalion/io/photogrammetry.py
@@ -3,6 +3,7 @@ import cedalion.dataclasses as cdc
 import numpy as np
 from collections import OrderedDict
 
+
 def read_photogrammetry_einstar(fn):
     """Read an output file of optodes and fiucials as returned by the
     photogrammetry pipeline using an einstar device.
@@ -42,14 +43,14 @@ def read_einstar(fn):
         The optodes as an OrderedDict.
     """
 
-    with open(fn, 'r') as f:
-        lines = [[l.strip() for l in line.split(',')] for line in f.readlines()]
+    with open(fn, "r") as f:
+        lines = [[l.strip() for l in line.split(",")] for line in f.readlines()]
         lines = [[line[0], [float(l) for l in line[1:]]] for line in lines]
-    assert lines[0][0] == 'Nz'
-    assert lines[1][0] == 'Iz'
-    assert lines[2][0] == 'Rpa'
-    assert lines[3][0] == 'Lpa'
-    assert lines[4][0] == 'Cz'
+    assert lines[0][0] == "Nz"
+    assert lines[1][0] == "Iz"
+    assert lines[2][0] == "Rpa"
+    assert lines[3][0] == "Lpa"
+    assert lines[4][0] == "Cz"
     fiducials = OrderedDict(lines[:5])
     optodes = OrderedDict(lines[5:])
     return fiducials, optodes
@@ -75,19 +76,29 @@ def opt_fid_to_xr(fiducials, optodes):
 
     # FIXME: this should get a different CRS
     CRS = "ijk"
-    fiducials = cdc.build_labeled_points(np.array(list(fiducials.values())),
-                                     labels=list(fiducials.keys()),
-                                     crs=CRS)#, units="mm")
-    opt = np.array(list(optodes.values()))
+    if len(fiducials) == 0:
+        fidu_coords = np.zeros((0, 3))
+    else:
+        fidu_coords = np.array(list(fiducials.values()))
+
+    if len(optodes) == 0:
+        opt_coords = np.zeros((0, 3))
+    else:
+        opt_coords = np.array(list(optodes.values()))
+
+    fiducials = cdc.build_labeled_points(
+        fidu_coords, labels=list(fiducials.keys()), crs=CRS
+    )  # , units="mm")
+
     types = []
     for lab in list(optodes.keys()):
-        if lab.startswith('S'):
+        if lab.startswith("S"):
             types.append(cdc.PointType(1))
-        elif lab.startswith('D'):
+        elif lab.startswith("D"):
             types.append(cdc.PointType(2))
         else:
             types.append(cdc.PointType(0))
-    optodes = cdc.build_labeled_points(opt,
-                                    labels=list(optodes.keys()),
-                                    crs=CRS, types=types)
+    optodes = cdc.build_labeled_points(
+        opt_coords, labels=list(optodes.keys()), crs=CRS, types=types
+    )
     return fiducials, optodes
--- tests/test_io_photogrammetry.py
+++ tests/test_io_photogrammetry.py
@@ -10,36 +10,50 @@ from numpy.testing import assert_array_almost_equal
 
 def test_read_einstar():
     # prepare test data
-    fiducials = np.random.rand(5,3)
-    optodes = np.random.rand(np.random.randint(100),3)
+    fiducials = np.random.rand(5, 3)
+    optodes = np.random.rand(np.random.randint(100), 3)
     tmp_fn = write_test_photo_fn(fiducials, optodes)
     # call read_einstar to read test data
     fid, opt = cedalion.io.read_einstar(tmp_fn)
     # evaluate
-    assert list(fid.keys()) == ['Nz', 'Iz', 'Rpa', 'Lpa', 'Cz']
+    assert list(fid.keys()) == ["Nz", "Iz", "Rpa", "Lpa", "Cz"]
     assert_array_almost_equal(np.array(list(fid.values())), fiducials)
     assert_array_almost_equal(np.array(list(opt.values())), optodes)
 
+
 def test_opt_fid_to_xr():
-    fiducials = np.random.rand(np.random.randint(10),3)
-    fiducials = {'F'+str(i): f for i, f in enumerate(fiducials)}
-    optodes = np.random.rand(np.random.randint(10),3)
-    optodes = {'S'+str(i): o for i, o in enumerate(optodes)}
-    fid, opt = opt_fid_to_xr(fiducials, optodes)
-    assert isinstance(fid, xr.DataArray)
-    assert isinstance(opt, xr.DataArray)
-    assert_array_almost_equal(fid.values, list(fiducials.values()))
-    assert_array_almost_equal(opt.values, list(optodes.values()))
-    assert (opt.type == cdc.PointType.SOURCE).all()
-    optodes = {l.replace('S', 'D'): o for l, o in optodes.items()}
-    fid, opt = opt_fid_to_xr(fiducials, optodes)
-    assert (opt.type == cdc.PointType.DETECTOR).all()
-    assert (list(opt.label.values) == ['D%d' % i for i in range(len(opt))])
+    for n_fidu in range(10):
+        for n_opts in range(10):
+            fiducials = np.random.rand(n_fidu, 3)
+            fiducials = {"F" + str(i): f for i, f in enumerate(fiducials)}
+            optodes = np.random.rand(n_opts, 3)
+
+            optodes = {"S" + str(i): o for i, o in enumerate(optodes)}
+            fid, opt = opt_fid_to_xr(fiducials, optodes)
+            assert isinstance(fid, xr.DataArray)
+            assert isinstance(opt, xr.DataArray)
+
+            if n_fidu > 0:
+                assert_array_almost_equal(fid.values, list(fiducials.values()))
+            else:
+                assert fid.shape == (0, 3)
+
+            if n_opts > 0:
+                assert_array_almost_equal(opt.values, list(optodes.values()))
+            else:
+                assert opt.shape == (0, 3)
+
+            assert (opt.type == cdc.PointType.SOURCE).all()
+            optodes = {l.replace("S", "D"): o for l, o in optodes.items()}
+            fid, opt = opt_fid_to_xr(fiducials, optodes)
+            assert (opt.type == cdc.PointType.DETECTOR).all()
+            assert list(opt.label.values) == ["D%d" % i for i in range(len(opt))]
+
 
 def test_read_photogrammetry_einstar():
     # prepare test data
-    fiducials = np.random.rand(5,3)
-    optodes = np.random.rand(100,3)
+    fiducials = np.random.rand(5, 3)
+    optodes = np.random.rand(100, 3)
     tmp_fn = write_test_photo_fn(fiducials, optodes)
     fid, opt = read_photogrammetry_einstar(tmp_fn)
     assert isinstance(fid, xr.DataArray)
@@ -53,19 +67,17 @@ def test_read_photogrammetry_einstar():
 def write_test_photo_fn(fid, opt):
     """Write a test file for photogrammetry data"""
     dirpath = tempfile.mkdtemp()
-    tmp_fn = os.path.join(dirpath, 'tmp.txt')
-    with open(tmp_fn, 'w') as f:
-        f.write('Nz,%f, %f, %f\n'  % (fid[0][0], fid[0][1], fid[0][2]))
-        f.write('Iz,%f, %f, %f\n'  % (fid[1][0], fid[1][1], fid[1][2]))
-        f.write('Rpa,%f, %f, %f\n' % (fid[2][0], fid[2][1], fid[2][2]))
-        f.write('Lpa,%f, %f, %f\n' % (fid[3][0], fid[3][1], fid[3][2]))
-        f.write('Cz,%f, %f, %f\n'  % (fid[4][0], fid[4][1], fid[4][2]))
+    tmp_fn = os.path.join(dirpath, "tmp.txt")
+    with open(tmp_fn, "w") as f:
+        f.write("Nz,%f, %f, %f\n" % (fid[0][0], fid[0][1], fid[0][2]))
+        f.write("Iz,%f, %f, %f\n" % (fid[1][0], fid[1][1], fid[1][2]))
+        f.write("Rpa,%f, %f, %f\n" % (fid[2][0], fid[2][1], fid[2][2]))
+        f.write("Lpa,%f, %f, %f\n" % (fid[3][0], fid[3][1], fid[3][2]))
+        f.write("Cz,%f, %f, %f\n" % (fid[4][0], fid[4][1], fid[4][2]))
         for i, v in enumerate(opt):
             if i < 10:
                 label = "S%d" % i
             else:
                 label = "D%d" % i
-            f.write('%s, %f, %f, %f\n' % (label, v[0], v[1], v[2]))
+            f.write("%s, %f, %f, %f\n" % (label, v[0], v[1], v[2]))
     return tmp_fn
-
-
