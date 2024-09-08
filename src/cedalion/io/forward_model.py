32787a77bf99681a132f24ce1b907913cc8e0ee6
Eike Middell <eike@middell.net>
2024-09-08 08:03:06 +0200

    facilitate saving fluence in h5-files with lossy compression

--- src/cedalion/io/forward_model.py
+++ src/cedalion/io/forward_model.py
@@ -1,5 +1,8 @@
+import h5py
 import xarray as xr
-import cedalion
+
+import cedalion.dataclasses as cdc
+
 
 def save_Adot(fn: str, Adot: xr.DataArray):
     """Save Adot to a netCDF file.
@@ -41,3 +44,77 @@ def load_Adot(fn: str):
                     }
             )
     return Adot
+
+
+
+def save_fluence(fn : str, fluence_all, fluence_at_optodes):
+    """Save forward model computation results.
+
+    This method uses a lossy compressions algorithm to reduce file size.
+    """
+
+    with h5py.File(fn, "w") as f:
+        # with scaleoffset=14 this should be precise to the 14th decimal digit.
+        f.create_dataset(
+            "fluence_all",
+            data=fluence_all,
+            scaleoffset=14,
+            shuffle=True,
+            compression="lzf",
+        )
+
+        f["fluence_all"].attrs["dims"] = fluence_all.dims
+        f["fluence_all"].attrs["label"] = [str(i) for i in fluence_all.label.values]
+        f["fluence_all"].attrs["wavelength"] = fluence_all.wavelength
+        f["fluence_all"].attrs["type"] = [i.value for i in fluence_all.type.values]
+
+        f.create_dataset(
+            "fluence_at_optodes",
+            data=fluence_at_optodes,
+            shuffle=True,
+            compression="lzf",
+        )
+
+        f["fluence_at_optodes"].attrs["dims"] = fluence_at_optodes.dims
+        f["fluence_at_optodes"].attrs["optode1"] = [
+            str(i) for i in fluence_at_optodes.optode1.values
+        ]
+        f["fluence_at_optodes"].attrs["optode2"] = [
+            str(i) for i in fluence_at_optodes.optode2.values
+        ]
+        f["fluence_at_optodes"].attrs["wavelength"] = fluence_all.wavelength
+
+        f.flush()
+
+
+def load_fluence(fn : str):
+    """Load forward model computation results."""
+
+    with h5py.File(fn, "r") as f:
+
+        ds = f["fluence_all"]
+        fluence_all = xr.DataArray(
+            ds,
+            dims = ds.attrs["dims"],
+            coords = {
+                "label" : ("label", ds.attrs["label"]),
+                "type" : ("label", [cdc.PointType(i) for i in ds.attrs["type"]]),
+                "wavelength" : ds.attrs["wavelength"]
+            }
+        )
+        fluence_all.attrs.clear()
+
+        ds = f["fluence_at_optodes"]
+
+        fluence_at_optodes = xr.DataArray(
+            ds,
+            dims = ds.attrs["dims"],
+            coords = {
+                "optode1" : ds.attrs["optode1"],
+                "optode2" : ds.attrs["optode2"],
+                "wavelength" : ds.attrs["wavelength"]
+            }
+        )
+        fluence_at_optodes.attrs.clear()
+
+    return fluence_all, fluence_at_optodes
