460c05f27407ef6471ff0a041f646bb96cb62075
lauracarlton <laura.carlton@mail.mcgill.ca>
2024-11-01 13:50:54 +0100

    code for parsing a dataset into BIDS components
    first attempt at a script to be run from the command line that takes a path to a dataset as input and finds all the snirf files in that directory. Then for each snirf file checks the filename and all parent directories for information on the bids fields (new function in cedalion/io/bids). If it doesn't anything it uses None. Then adds this info as a row per snirf file in a csv file that is saved in the dataset directory.

--- src/cedalion/io/bids.py
+++ src/cedalion/io/bids.py
@@ -5,3 +5,17 @@ import pandas as pd
 
 def read_events_from_tsv(fname: str | Path):
     return pd.read_csv(fname, delimiter="\t")
+
+
+def check_for_bids_field(path_parts: list, 
+                         field: str):
+    
+    field_parts = [part for part in path_parts if field in part]
+    if len(field_parts) == 0:
+        value = None
+    else:
+        find_value = field_parts[-1].split('_') # assume the lowest directory level supersedes any higher directory level ? not sure if we should assume this 
+        value = [vals for vals in find_value if field in vals][0]
+        value = value.split('-')[1]
+        
+    return value
