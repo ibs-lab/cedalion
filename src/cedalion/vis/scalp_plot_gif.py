b811b6d7e15bee1bf7f53a2f43d4b949e8757848
Alexander von Lühmann <avonluh@gmail.com>
2025-04-09 20:01:16 +0200

    updated code and docstrings

--- src/cedalion/vis/scalp_plot_gif.py
+++ src/cedalion/vis/scalp_plot_gif.py
@@ -4,51 +4,49 @@ import cedalion.plots as plots
 import matplotlib.pyplot as p
 import numpy as np
 import imageio
+import cedalion.typing as cdt
 from PIL import Image
 
 
 
-def scalp_plot_gif( data_ts, geo3d, frame_range, filename, scl=None, fps=10, optode_size=6, optode_labels=False, str_title=''):
-    """
-    Generate a GIF of scalp topographies over time from time-series data.
+def scalp_plot_gif( data_ts: cdt.NDTimeSeries, geo3d: cdt.LabeledPointCloud, frame_range, 
+                   filename: str, scl=None, fps: int =10, optode_size: float = 6, optode_labels: bool =False, 
+                   str_title: str =''):
+    """Generate a GIF of scalp topographies over time from time-series data.
 
-    Parameters
-    ----------
-    data_ts : xarray.DataArray
-        A 2D DataArray with dimensions (channel, time). Must include coordinate labels
-        for 'source' and 'detector' in the 'channel' dimension.
-    
-    geo3d : cedalion.core.LabeledPointCloud
-        3D geometry object defining optode locations for projecting onto the scalp surface.
-    
-    frame_range : tuple of (int, int, int)
-        A (start, stop, step) tuple specifying the time frame indices to include in the GIF.
-    
-    filename : str
-        Full path to the output GIF file.
-    
-    scl : tuple of (float, float), optional
-        Tuple defining the (vmin, vmax) for the color scale. If None, the color scale is set
-        to ± the maximum absolute value of the data.
-    
-    fps : int, optional
-        Frames per second for the output GIF. Default is 10.
-    
-    optode_size : float, optional
-        Size of optode markers on the plot. Default is 6.
-    
-    optode_labels : bool, optional
-        Whether to show text labels for optodes instead of markers. Default is False.
-    
-    str_title : str, optional
-        Extra string to append to the title of each frame.
+    Args:
+        data_ts : xarray.DataArray
+            A 2D DataArray with dimensions (channel, time). Must include coordinate labels
+            for 'source' and 'detector' in the 'channel' dimension.
+        geo3d : cedalion.core.LabeledPointCloud
+            3D geometry object defining optode locations for projecting onto the scalp surface.
+        frame_range : tuple of (int, int, int)
+            A (start, stop, step) tuple specifying the time frame indices to include in the GIF.
+        filename : str
+            Full path to the output GIF file without file extension.
+        scl : tuple of (float, float), optional
+            Tuple defining the (vmin, vmax) for the color scale. If None, the color scale is set
+            to ± the maximum absolute value of the data.
+        fps : int, optional
+            Frames per second for the output GIF. Default is 10.
+        optode_size : float, optional
+            Size of optode markers on the plot. Default is 6.
+        optode_labels : bool, optional
+            Whether to show text labels for optodes instead of markers. Default is False.
+        str_title : str, optional
+            Extra string to append to the title of each frame.
 
-    Returns
-    -------
+    Returns:
     None
         The function saves a GIF file to the specified location.
+
+    Initial Contributors:
+    - David Boas | dboas@bu.edu | 2025
+    - Alexander von Lühmann | vonluehmann@tu-berlin.de | 2025
     """
-    
+
+    filename = filename+'.gif'
+
     if scl is None:
         scl = (-np.max(np.abs(data_ts.values)),np.max(np.abs(data_ts.values)))
 
@@ -63,7 +61,7 @@ def scalp_plot_gif( data_ts, geo3d, frame_range, filename, scl=None, fps=10, opt
 
         ax1.cla()
         ax1.set_position([0.1, 0.1, 0.8, 0.8])  # reset position to avoid inset growth from colorbar
-        plots.scalp_plot( 
+        plots.scalp_plot(
             data_ts,
             geo3d,
             data_ts.isel(time=idx_frame).values,
@@ -81,4 +79,3 @@ def scalp_plot_gif( data_ts, geo3d, frame_range, filename, scl=None, fps=10, opt
         frames.append(image)
 
     frames[0].save(filename, save_all=True, append_images=frames[1:], duration=1000/fps, loop=0)
-
--- src/cedalion/vis/surface_multi_view.py
+++ src/cedalion/vis/surface_multi_view.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as p
 import pyvista as pv
 from matplotlib.colors import ListedColormap
-
+import cedalion.typing as cdt
+from cedalion.imagereco.forward_model import TwoSurfaceHeadModel
 import numpy as np
 
 import cedalion.dataclasses as cdc 
 
 
 
-def plot_image_recon( X, head, shape, iax, clim=(0,1), flag_hbx='hbo_brain', view_position='superior', p0 = None, title_str = None, off_screen= True ):
+def plot_image_recon( X: cdt.NDTimeSeries, head: TwoSurfaceHeadModel, shape, iax, clim=(0,1), flag_hbx='hbo_brain', view_position='superior',
+                     p0 = None, title_str = None, off_screen= True ):
 
-    cmap = p.get_cmap("jet", 256)
+    cmap = p.get_cmap("seismic", 1024) # or "jet"
     new_cmap_colors = np.vstack((cmap(np.linspace(0, 1, 256))))
     custom_cmap = ListedColormap(new_cmap_colors)
 
@@ -20,31 +22,19 @@ def plot_image_recon( X, head, shape, iax, clim=(0,1), flag_hbx='hbo_brain', vie
     X_hbo_scalp = X[~X.is_brain.values, 0]
     X_hbr_scalp = X[~X.is_brain.values, 1]
 
-    pos_names = ['superior', 'left', 'right', 'anterior', 'posterior','scale_bar']
-    positions = [ 'xy',
-        [(-400., 96., 130.),
-        (96., 115., 165.),
-        (0,0,1)],
-        [(600, 96., 130.),
-        (96., 115., 165.),
-        (0,0,1)],
-        [(100, 500, 200),
-        (96., 115., 165.),
-        (0,0,1)],
-        [(100, -300, 300),
-        (96., 115., 165.),
-        (0,0,1)],
-        [(100, -300, 300),
-        (96., 115., 165.),
-        (0,0,1)]
-    ]
+    positions = {
+        'superior': [0, 0, 1],
+        'left': [-1, 0, 0],
+        'right': [1, 0, 0],
+        'anterior': [0, 1, 0],
+        'posterior': [0, -1, 0],
+        'scale_bar': [0, 0, 1]
+    }
     # FIXME provide clim if not provided
     #clim=(-X_hbo_brain.max(), X_hbo_brain.max())
 
-    # get index of pos_names that matches view_position
-    idx = [i for i, s in enumerate(pos_names) if view_position in s]
-
-    pos = positions[idx[0]]
+    # Get the camera direction vector for the specified view_position
+    camera_direction = positions.get(view_position, [0, 0, 1])
 
     if p0 is None:
         p0 = pv.Plotter(shape=(shape[0],shape[1]), window_size = [2000, 1500], off_screen=off_screen)
@@ -54,40 +44,59 @@ def plot_image_recon( X, head, shape, iax, clim=(0,1), flag_hbx='hbo_brain', vie
     show_scalar_bar = False
     smooth_shading = True
 
-    if flag_hbx == 'hbo_brain': # hbo brain 
+    if flag_hbx in ['hbo_brain', 'hbr_brain']:
         surf = cdc.VTKSurface.from_trimeshsurface(head.brain)
-        surf = pv.wrap(surf.mesh)
+    elif flag_hbx in ['hbo_scalp', 'hbr_scalp']:
+        surf = cdc.VTKSurface.from_trimeshsurface(head.scalp)
+    else:
+        raise ValueError(f"Invalid flag_hbx: {flag_hbx}")
+    surf = pv.wrap(surf.mesh)
+
+    # Calculate the centroid of the mesh
+    centroid = np.mean(surf.points, axis=0)
+
+    if flag_hbx == 'hbo_brain': # hbo brain 
         surf['brain'] = X_hbo_brain
         if clim is None:
            clim=(-X_hbo_brain.max(), X_hbo_brain.max())
         # FIXME propagate this surf change to other HbXs and Scalp
 #        p0.add_mesh(surf, scalars=X_hbo_brain, cmap=custom_cmap, clim=clim, show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9), smooth_shading=smooth_shading, interpolate_before_map=False )
-        p0.add_mesh(surf, scalars='brain', cmap=custom_cmap, clim=clim, show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9), smooth_shading=smooth_shading, interpolate_before_map=False )
-        p0.camera_position = pos
-
+        p0.add_mesh(surf, scalars='brain', cmap=custom_cmap, clim=clim,
+                    show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9), 
+                    smooth_shading=smooth_shading, interpolate_before_map=False )
     elif flag_hbx == 'hbr_brain': # hbr brain
-        surf = cdc.VTKSurface.from_trimeshsurface(head.brain)
-        surf = pv.wrap(surf.mesh)   
+        surf['brain'] = X_hbr_brain
         if clim is None:
            clim=(-X_hbr_brain.max(), X_hbr_brain.max())
-        p0.add_mesh(surf, scalars=X_hbr_brain, cmap=custom_cmap, clim=clim, show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9), smooth_shading=smooth_shading )
-        p0.camera_position = pos
-
+        p0.add_mesh(surf, scalars=X_hbr_brain, cmap=custom_cmap, clim=clim,
+                    show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9),
+                    smooth_shading=smooth_shading )
     elif flag_hbx == 'hbo_scalp': # hbo scalp
-        surf = cdc.VTKSurface.from_trimeshsurface(head.scalp)
-        surf = pv.wrap(surf.mesh)
+        surf['brain'] = X_hbo_scalp
         if clim is None:
             clim=(-X_hbo_scalp.max(), X_hbo_scalp.max())
-        p0.add_mesh(surf, scalars=X_hbo_scalp, cmap=custom_cmap, clim=clim, show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9), smooth_shading=smooth_shading )
-        p0.camera_position = pos
-
+        p0.add_mesh(surf, scalars=X_hbo_scalp, cmap=custom_cmap, clim=clim,
+                    show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9),
+                    smooth_shading=smooth_shading )
     elif flag_hbx == 'hbr_scalp': # hbr scalp
-        surf = cdc.VTKSurface.from_trimeshsurface(head.scalp)
-        surf = pv.wrap(surf.mesh)
+        surf['brain'] = X_hbr_scalp
         if clim is None:
             clim=(-X_hbr_scalp.max(), X_hbr_scalp.max())
-        p0.add_mesh(surf, scalars=X_hbr_scalp, cmap=custom_cmap, clim=clim, show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9), smooth_shading=smooth_shading )
-        p0.camera_position = pos
+        p0.add_mesh(surf, scalars=X_hbr_scalp, cmap=custom_cmap, clim=clim,
+                    show_scalar_bar=show_scalar_bar, nan_color=(0.9,0.9,0.9),
+                    smooth_shading=smooth_shading )
+
+    # Determine an appropriate view up vector based on view_position
+    if view_position == 'superior':
+        view_up = [0, 1, 0]  # Use a lateral view-up vector for top-down view
+    else:
+        view_up = [0, 0, 1]  # Default view up vector
+
+    p0.camera_position = [
+        centroid + np.array(camera_direction) * 500,  # Camera position
+        centroid,                                     # Focal point (centroid)
+        view_up                                       # Updated view up direction
+    ]
 
     if iax[0] == 1 and iax[1] == 1:
         p0.clear_actors()
@@ -103,54 +112,49 @@ def plot_image_recon( X, head, shape, iax, clim=(0,1), flag_hbx='hbo_brain', vie
 
 
 
-def surface_multi_view( X_ts, head, clim=None, view_type='hbo_brain', title_str=None, filename=None, SAVE=False, SAVE_GIF=False, frame_indices=None, fps=6 ):
-    """
-    Generate a multi-view 3D visualization of brain or scalp data projected onto a head mesh,
-    with optional saving as a static image or animated GIF over time.
+def surface_multi_view( X_ts: cdt.NDTimeSeries, head: TwoSurfaceHeadModel, clim=None,
+                       view_type: str ='hbo_brain', title_str: str=None,
+                       filename: str =None, SAVE: bool = False, SAVE_GIF: bool =False,
+                       frame_indices = None, fps: int =6 ):
+    """Generate a multi-view 3D visualization of brain or scalp data projected onto a head mesh, with optional saving as a static image or animated GIF over time.
 
-    Parameters
-    ----------
-    X_ts : xarray.DataArray
-        Functional image data. Must be 2D (vertex × chromo) or 3D (vertex × chromo × time).
-        Requires 'vertex' and 'chromo' dimensions, and optionally 'time'.
+    Args:
+        X_ts : xarray.DataArray
+            Functional image data. Must be 2D (vertex × chromo) or 3D (vertex × chromo × time).
+            Requires 'vertex' and 'chromo' dimensions, and optionally 'time'.
+        head : object
+            A head surface mesh object with attributes `.brain` and `.scalp` containing
+            `trimesh`-compatible geometry.
+        clim : tuple of (float, float), optional
+            Color limits for the visualization. If None, it will be automatically set based on the data.
+        view_type : str, optional
+            Type of signal and surface to display. Options include:
+            - 'hbo_brain', 'hbr_brain'
+            - 'hbo_scalp', 'hbr_scalp'
+            Default is 'hbo_brain'.
+        title_str : str, optional
+            Title string for the scalar bar in the central panel (used when adding a scale bar).
+        filename : str, optional
+            Path and base name for saving the static PNG image or animated GIF without file extension. 
+            Required if SAVE or SAVE_GIF is True.
+        SAVE : bool, optional
+            If True, saves a static image of the multi-view layout as `<filename>.png`.
+        SAVE_GIF : bool, optional
+            If True, generates an animated GIF of the changing signal over time, saving to `<filename>.gif`.
+            Requires X_ts to have a 'time' dimension and `frame_indices` to be specified.
+        frame_indices : tuple of (int, int, int), optional
+            Tuple specifying the frame range for the GIF: (start_index, stop_index, step).
+            Required if SAVE_GIF is True.
+        fps : int, optional
+            Frames per second for the animated GIF. Default is 6.
 
-    head : object
-        A head surface mesh object with attributes `.brain` and `.scalp` containing
-        `trimesh`-compatible geometry.
+    Returns:
+        None
+            Displays or saves the multi-view rendering. Returns nothing.
 
-    clim : tuple of (float, float), optional
-        Color limits for the visualization. If None, it will be automatically set based on the data.
-
-    view_type : str, optional
-        Type of signal and surface to display. Options include:
-        - 'hbo_brain', 'hbr_brain'
-        - 'hbo_scalp', 'hbr_scalp'
-        Default is 'hbo_brain'.
-
-    title_str : str, optional
-        Title string for the scalar bar in the central panel (used when adding a scale bar).
-
-    filename : str, optional
-        Path and base name for saving the static PNG image or animated GIF. Required if SAVE or SAVE_GIF is True.
-
-    SAVE : bool, optional
-        If True, saves a static image of the multi-view layout as `<filename>.png`.
-
-    SAVE_GIF : bool, optional
-        If True, generates an animated GIF of the changing signal over time, saving to `<filename>.gif`.
-        Requires X_ts to have a 'time' dimension and `frame_indices` to be specified.
-
-    frame_indices : tuple of (int, int, int), optional
-        Tuple specifying the frame range for the GIF: (start_index, stop_index, step).
-        Required if SAVE_GIF is True.
-
-    fps : int, optional
-        Frames per second for the animated GIF. Default is 6.
-
-    Returns
-    -------
-    None
-        Displays or saves the multi-view rendering. Returns nothing.
+    Initial Contributors:
+    - David Boas | dboas@bu.edu | 2025
+    - Alexander von Lühmann | vonluehmann@tu-berlin.de | 2025
     """
 
     subplot_shape = (2,3)
@@ -200,7 +204,7 @@ def surface_multi_view( X_ts, head, clim=None, view_type='hbo_brain', title_str=
         if frame_indices is None:
             print('No frame_indices provided. Please provide frame_indices=(idx_start,idx_stop,idx_step).')
             return
-        
+
         p0.open_gif( filename+'.gif', fps=6 )
 
         # check if view_type contains 'hbo'
@@ -211,7 +215,7 @@ def surface_multi_view( X_ts, head, clim=None, view_type='hbo_brain', title_str=
 
         foo_v = np.full([X_ts.vertex.size, 1], np.nan)
         for idx_frame in range(frame_indices[0],frame_indices[1],frame_indices[2]):
-                
+
             foo_v[:,0] = X_ts.sel(chromo=HbX).isel(time=idx_frame).values
 
             surf1['brain'] = foo_v[X_ts.is_brain,0]
