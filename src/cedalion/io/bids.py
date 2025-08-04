735a195b2763d6a0b3fbc76af737cf08ab440df7
shakiba93 <shakiba.mrd71@gmail.com>
2025-08-04 15:16:23 +0200

    trim datetime miliseconds info in acq_time

--- src/cedalion/io/bids.py
+++ src/cedalion/io/bids.py
@@ -263,7 +263,7 @@ def search_for_acq_time_in_snirf_files(row: pd.Series, dataset_path: str) -> dat
         nirs_data = next(iter(nirs_data.nirs))
 
         datetime_str = f"{nirs_data.metaDataTags.MeasurementDate} {nirs_data.metaDataTags.MeasurementTime}"
-        timestamp = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+        timestamp = datetime.strptime(datetime_str.split('.')[0], "%Y-%m-%d %H:%M:%S")
         return timestamp
     else:
         return row.acq_time
